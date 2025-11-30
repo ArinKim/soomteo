2c4b426cb587c3b87bcb12dcd8735a15784fcf06
dbehdgns1215 <dbehdgns1215@naver.com>
2025-11-30 20:52:00 +0900

    Feat: Add refresh token rotate feature - 갱신된 사용자 리프레시 토큰 값, 기존 리프레시 토큰의 유효기간이 1개월 미만인 경우에만 갱신 (카카오 API)

---
+++ backend/spring/backend/src/main/java/com/soomteo/backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.soomteo.backend.config;
+
+import com.soomteo.backend.oauth.interceptor.TokenRefreshInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final TokenRefreshInterceptor tokenRefreshInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenRefreshInterceptor)
+                .addPathPatterns("/api/v1/auth/main") // 메인 페이지만
+                .excludePathPatterns("/api/v1/auth/kakao/login", "/api/v1/auth/kakao/callback");
+    }
+}
